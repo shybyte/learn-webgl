263cb88decf5e9d43fc5e103e6ffba719a4d7bd1
shybyte <shybyte@users.noreply.github.com>
2022-10-27 01:17:41 +0200

    finished tut-11-diffuse-lightning.ts

--- invent-box/tut-11-diffuse-lightning.ts
+++ invent-box/tut-11-diffuse-lightning.ts
@@ -84,6 +84,17 @@ export function main() {
 
   const uvBuffer = createMyBuffer(gl, uvData, 2);
 
+  // F|L|B|R|T|U
+  const normalData = [
+    ...repeat(6, [0, 0, 1]),    // Z+
+    ...repeat(6, [-1, 0, 0]),   // X-
+    ...repeat(6, [0, 0, -1]),   // Z-
+    ...repeat(6, [1, 0, 0]),    // X+
+    ...repeat(6, [0, 1, 0]),    // Y+
+    ...repeat(6, [0, -1, 0]),   // Y-
+  ];
+  const normalBuffer = createMyBuffer(gl, normalData, 3);
+
   const TEXTURE_ID = 0;
   loadAndBindTexture(gl, `textures/invent-box-logo-512px.jpg`, TEXTURE_ID);
 
@@ -91,15 +102,24 @@ export function main() {
   const program = createProgram(gl, `
       precision mediump float;
 
+      const vec3 lightDirection = normalize(vec3(0, 1, 1));
+      const float ambient = 0.1;
+      
       attribute vec3 position;
       attribute vec2 uv;
+      attribute vec3 normal;
 
       varying vec2 vUV;
+      varying float vBrightness;
 
       uniform mat4 matrix;
+      uniform mat4 normalMatrix;
 
       void main() {
+        vec3 worldNormal = (normalMatrix * vec4(normal, 1)).xyz;
+        float diffuse = max(0.0, dot(worldNormal, lightDirection));
         vUV = uv;
+        vBrightness = ambient + diffuse;
         gl_Position = matrix * vec4(position, 1);
       }`
     ,
@@ -107,10 +127,13 @@ export function main() {
       precision mediump float;
 
       varying vec2 vUV;
+      varying float vBrightness;
       uniform sampler2D textureID;
 
       void main() {
-        gl_FragColor = texture2D(textureID, vUV);
+        vec4 texel = texture2D(textureID, vUV);
+        texel.xyz *= vBrightness;
+        gl_FragColor = texel;
       }`
   );
 
@@ -119,9 +142,11 @@ export function main() {
 
   setProgramAttributeToMyBuffer(gl, program, 'position', positionBuffer);
   setProgramAttributeToMyBuffer(gl, program, 'uv', uvBuffer);
+  setProgramAttributeToMyBuffer(gl, program, 'normal', normalBuffer);
 
   const uniformLocations = {
     matrix: gl.getUniformLocation(program, 'matrix'),
+    normalMatrix: gl.getUniformLocation(program, `normalMatrix`),
     textureID: gl.getUniformLocation(program, 'textureID'),
   };
 
@@ -143,6 +168,8 @@ export function main() {
   mat4.translate(viewMatrix, viewMatrix, [0, -0, 1]);
   mat4.invert(viewMatrix, viewMatrix);
 
+  const normalMatrix = mat4.create();
+
   const mvMatrix = mat4.create();
   const finalMatrix = mat4.create();
 
@@ -152,8 +179,15 @@ export function main() {
     mat4.rotateZ(modelMatrix, modelMatrix, Math.PI / 2 / 100);
     mat4.rotateX(modelMatrix, modelMatrix, Math.PI / 2 / 70);
     mat4.multiply(mvMatrix, viewMatrix, modelMatrix);
+
+    mat4.invert(normalMatrix, mvMatrix);
+    mat4.transpose(normalMatrix, normalMatrix);
+
     mat4.multiply(finalMatrix, projectionMatrix, mvMatrix);
+
     gl.uniformMatrix4fv(uniformLocations.matrix, false, finalMatrix);
+    gl.uniformMatrix4fv(uniformLocations.normalMatrix, false, normalMatrix);
+
     gl.drawArrays(gl.TRIANGLES, 0, positionBuffer.length);
   }
 
